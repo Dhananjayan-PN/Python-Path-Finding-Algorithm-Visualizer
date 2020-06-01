cfdf8dbd3b69b6de0db3db29aaa344a11a076b9a
Dhananjayan P N <pndjay@gmail.com>
2020-06-01 19:43:08 +0530

    gui setup

--- interface.py
+++ interface.py
@@ -1,4 +1,7 @@
+from tkinter import *
 import pygame
+import platform
+import os
 
 
 class Node:
@@ -21,7 +24,9 @@ class Interface:
         print("Initializing user interface...")
 
 
-gridSize = 16
+gridSize = 20
+blockSize = 40
+CYAN = (21, 244, 238)
 nodes = []
 for i in range(gridSize):
     nodeRow = [Node(i, j) for j in range(gridSize)]
@@ -30,8 +35,18 @@ grid = Grid(gridSize, nodes)
 interface = Interface(grid)
 interface.start()
 
+root = Tk()
+embed = Frame(root, width=1280,
+              height=800)  #creates embed frame for pygame window
+embed.grid(columnspan=(600), rowspan=500)  # Adds grid
+embed.pack(side=LEFT)  #packs window to the left
+# buttonwin = Frame(root, width=75, height=500)
+# buttonwin.pack(side=LEFT)
+os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
+if platform.system == "Windows":
+    os.environ['SDL_VIDEODRIVER'] = 'windib'
 pygame.init()
-screen = pygame.display.set_mode([1200, 800])
+screen = pygame.display.set_mode([800, 800])
 pygame.display.set_caption("Path FInding Algorithm Visualizer")
 icon = pygame.image.load('d_icon.ico')
 pygame.display.set_icon(icon)
@@ -42,7 +57,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-    pygame.display.flip()
+        for row in grid.nodes:
+            for node in row:
+                rect = pygame.Rect(node.x * blockSize, node.y * blockSize,
+                                   blockSize, blockSize)
+                pygame.draw.rect(screen, CYAN, rect, 1)
+    pygame.display.update()
+    root.update()
 
 pygame.quit()
